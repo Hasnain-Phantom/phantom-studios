a1a9335450b8756e507deacf6ee852f647c62cfc
AdeiaSaif <120249675+AdeiaSaif@users.noreply.github.com>
2024-12-15 00:41:02 +0500

    fixed issue

--- Phantom Studios/script.js
+++ Phantom Studios/script.js
@@ -6,14 +6,22 @@ document.querySelector("#main").addEventListener("mousemove",function(dets){
         ease:"back.out"
     })
 })
+document.querySelector("#main").addEventListener("mousemove",function(dets){
+    gsap.to(document.querySelector("#cursor"),{
+        x:dets.x,
+        y:dets.y,
+        duration:0.5,
+        ease:"back.out"
+    })
+})
 gsap.to("#Phantom h1",{
-    // transform:"translateX(-90%)",
+    transform:"translateX(-90%)",
     scrollTrigger:{
         trigger:"#Phantom",
         scoller:"body",
-        // markers:true,
-        // start:"top 0%",
-        // end:"top -100%",
+        markers:true,
+        start:"top 0%",
+        end:"top -100%",
         scrub:2,pin:true
     }
 })
