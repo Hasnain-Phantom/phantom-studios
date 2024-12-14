ef97f97a270be473d4c1fad8db15cf19f5cc91a2
AdeiaSaif <120249675+AdeiaSaif@users.noreply.github.com>
2024-12-15 00:38:29 +0500

    Adding files

---
+++ Phantom Studios/script.js
@@ -0,0 +1,19 @@
+document.querySelector("#main").addEventListener("mousemove",function(dets){
+    gsap.to(document.querySelector("#cursor"),{
+        x:dets.x,
+        y:dets.y,
+        duration:0.5,
+        ease:"back.out"
+    })
+})
+gsap.to("#Phantom h1",{
+    // transform:"translateX(-90%)",
+    scrollTrigger:{
+        trigger:"#Phantom",
+        scoller:"body",
+        // markers:true,
+        // start:"top 0%",
+        // end:"top -100%",
+        scrub:2,pin:true
+    }
+})
