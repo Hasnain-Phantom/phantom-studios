87f283942191bc97347123465d58b8bd2c0be69c
AdeiaSaif <120249675+AdeiaSaif@users.noreply.github.com>
2024-12-15 22:29:03 +0500

    header

--- Phantom Studios/script.js
+++ Phantom Studios/script.js
@@ -1,1 +1,23 @@
-    
+gsap.from("#phantom-logo",{
+    fontSize:"230px",
+    y:200,
+    scrollTrigger:{
+        trigger:"#logo",
+        scroll:"body",
+        start:"top 0",
+        end:"top 100%",
+        scrub:1
+    }
+})
+
+gsap.from("#menu",{
+   x:-300,
+
+    scrollTrigger:{
+        trigger:"#logo",
+        scroll:"body",
+        start:"top 0",
+        end:"top 100%",
+        scrub:1
+    }
+})
