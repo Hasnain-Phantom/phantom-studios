858ef820dd6cefe1dedab57742d24771446f0d6c
AdeiaSaif <120249675+AdeiaSaif@users.noreply.github.com>
2024-12-16 23:23:36 +0500

    Hero section

--- Phantom Studios/script.js
+++ Phantom Studios/script.js
@@ -1,24 +1,44 @@
-gsap.from("#phantom-logo",{
-    fontSize:"230px",
-    y:200,
-    scrollTrigger:{
-        trigger:"#logo",
-        scroll:"body",
-        start:"top 0",
-        end:"top 100%",
-        scrub:1
+gsap.to("#menuitems", {
+    y: -100,
+    scrollTrigger: {
+        trigger: ".navbar",
+        start: "center top",
+        end: "center 100%",
+        scrub: 1
+    },
+});
+gsap.from(".side-menu",{
+    x:50,  
+    scrollTrigger: {
+        trigger: ".navbar",
+        start: "center top",
+        end: "center 100%",
+        scrub: 1
+    },
+});
+
+gsap.from(".menu-icon",{
+    x:500,  
+    scrollTrigger: {
+        trigger: ".navbar",
+        start: "center top",
+        end: "center 100%",
+        scrub: 1
     }
-  })
-  
-  gsap.from("#menu",{
-   x:-300,
-  
-    scrollTrigger:{
-        trigger:"#logo",
-        scroll:"body",
-        start:"top 0",
-        end:"top 100%",
-        scrub:1
-    }
-  })
-  
+});
+var t1=gsap.timeline();
+t1
+.from("#phan", {
+    duration: 1,
+    opacity:0,
+    y:20
+})
+.from("#studio", {
+    duration: 1,
+    opacity:0,
+    y:20
+
+});
+
+
+
