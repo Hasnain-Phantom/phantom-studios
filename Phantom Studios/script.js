59a8fc579171d693b70ed6c64e956512b9436893
AdeiaSaif <120249675+AdeiaSaif@users.noreply.github.com>
2024-12-29 15:26:40 +0500

    Service section added

--- Phantom Studios/script.js
+++ Phantom Studios/script.js
@@ -3,18 +3,17 @@
 
 var laptop_menu_icon=document.getElementsByClassName("laptop-menu-icon")[0];
 laptop_menu_icon.addEventListener("click",function(){
-    document.getElementById("nav").style.display="none";
     gsap.to("#click-menu",{
         y:0,
         duration:1
     })
+    document.getElementById("nav").style.display="none";
 })
 
 var laptop_close_button=document.getElementsByClassName("laptop-close-button")[0];
 laptop_close_button.addEventListener("click",function(){
     gsap.to("#click-menu",{
         y:-800,
-        opacity:0,
         duration:1
     })
     document.getElementById("nav").style.display="block";
